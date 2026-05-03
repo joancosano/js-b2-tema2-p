f6472a14810f32dff8317997d1769f071d3c0bc2
joancosano <joan.cosano@gmail.com>
2026-05-03 23:12:01 +0200

    finished

--- u1/u1e1.js
+++ u1/u1e1.js
@@ -3,3 +3,43 @@
 // Enunciado disponible en u1e1.md / Enunciat disponible a u1e1.md
 
 //Escribe aquí tu solución / escriviu aquí la vostra solució:
+
+export class Validator {
+
+    #field;
+    #value;
+    #required;
+    
+    constructor(fieldName = '',value = '',required = false)
+    {
+        this.#field = fieldName;
+        this.#value = value;
+        this.#required = required;
+    }
+    get field(){
+        return this.#field
+    }
+    set field(name){
+        this.#field = name;
+    }
+    get value(){
+        return this.#value
+    }
+    set value(value){
+        this.#value = value;
+    }
+
+    get required(){
+        return this.#required
+    }
+    set required(required){
+        this.#required = required;
+    }
+    isEmpty(){
+        return this.#value === '';
+    }
+    isValid(){
+        return !this.isEmpty() || !this.required;
+    }
+
+}
