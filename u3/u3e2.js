08a2a386a2a05b283b62beb8d60958b5f1ee1fb9
j2 <j2@mac-book-pro-joan.local>
2026-05-15 16:09:20 +0200

    finished

--- u3/u3e2.js
+++ u3/u3e2.js
@@ -3,3 +3,41 @@
 // Enunciado disponible en u3e2.md / Enunciat disponible a u3e2.md
 
 //Escribe aquí tu solución / escriviu aquí la vostra solució:
+import { Shape } from "./u3e1.js";
+
+export class Rectangle extends Shape {
+
+    #base;
+    #height;
+
+    constructor(base = 0, height = 0){
+        super (2)
+        this.#base = base;
+        this.#height = height; 
+        
+    }
+    
+    get base(){
+        return this.#base
+    }
+    set base(base){
+        this.#base = base;
+    }
+    
+    get height(){
+        return this.#height
+    }
+
+    set height(height){
+        this.#height = height;
+    }
+
+    getArea(){
+        return (this.#base * this.#height)
+    }
+
+    isSquare(){
+        return this.#base === this.#height
+    }
+
+}
