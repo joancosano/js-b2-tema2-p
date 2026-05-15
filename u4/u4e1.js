7e88573d01aa13a986c99487d3e351d480a78882
j2 <j2@mac-book-pro-joan.local>
2026-05-15 17:28:26 +0200

    finished

--- u4/u4e1.js
+++ u4/u4e1.js
@@ -3,3 +3,38 @@
 // Enunciado disponible en u4e1.md / Enunciat disponible a u4e1.md
 
 //Escribe aquí tu solución / escriviu aquí la vostra solució:
+ 
+export const IPrintable = {
+
+    print(){}
+
+};
+
+Object.defineProperty(Object.prototype, "implements",{
+
+    enumerable: false,
+
+    value: function(interf){
+        for (const key in interf){
+            if (typeof this[key] !== "function"){
+                return false
+            }
+        }
+        return true
+    }
+});
+
+export class Message{
+    text; 
+    
+    constructor(text){
+        this.text = text;
+        
+        if (!this.implements(IPrintable)){
+            throw new Error('ERROR_IMPLEMENTS. This class doesn\'t implements the interface IPrintable.');
+        }}
+
+        print(){
+            console.log(this.text)
+        }
+}
